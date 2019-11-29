45c50fc844fb4487a1cf080ee57fd407adf8e780
liyassudeen <33513683+liyassudeen@users.noreply.github.com>
2019-11-29 17:20:42 +0530

    Parsing Req body
    Parsing the request body

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,9 +1,13 @@
 const homeModel = require("../model/homeModel");
 
-exports.home = (req, res) => {
+exports.getHome = (req, res) => {
   const homeModelData = new homeModel();
   res.render("home", {
     pageTitle: homeModelData.title,
     pageHeader: homeModelData.header
   });
+};
+
+exports.postHome = (req, res) => {
+  console.log(req.body.message);
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const homeController = require("./controller/homeController");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use("/", homeController.home);
+app.get("/", homeController.getHome);
+app.post("/form", homeController.postHome);
 app.listen(8080); //the server object listens on port 8080
